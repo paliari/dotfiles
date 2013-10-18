cd2780eda871d4040b39add1a1d2e0602da129bc
Marcos Neves <marcos.neves@gmail.com>
2013-10-18 09:49:54 -0300

    add install_sublime

--- mac_install.sh
+++ mac_install.sh
@@ -15,3 +15,9 @@ install_command_line_tools() {
 	open command_line_tools_os_x_mountain_lion_for_xcode*
 	open /Volumes/Command\ Line\ Tools\ \(Mountain\ Lion\)/Command\ Line\ Tools\ \(Mountain\ Lion\).mpkg
 }
+
+install_sublime() {
+	cd ~/Downloads
+	wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203047.dmg
+	open Sublime\ Text\ Build\ 3047.dmg
+}

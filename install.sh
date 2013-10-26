3d0fc580292dcb6d5b47d4ff5c68e714c0610dd0
Marcos Neves <marcos.neves@gmail.com>
2013-10-26 10:43:11 -0200

    update commandline tools and install_dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 DOTDIR=$HOME/.dotfiles
+
 if [ ! -e $DOTDIR ]
 then
   DOTFILES=$(cd `dirname $0` && pwd)
@@ -17,21 +19,29 @@ done
 echo "config .ssh"
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-cp $DOTDIR/ssh/authorized_keys ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/*
 
-# https://github.com/github/gitignore
-echo "global .gitignore"
-git config --global core.excludesfile ~/.gitignore
+cat <<'EOF' > ~/gitconfig-user
+[user]
+	email =
+	name =
+[github]
+	user =
+	token =
+EOF
 
-download_bin() {
-	echo "downloading $1"
-	curl -sL $2 > $DOTDIR/bin/$1
-	chmod 755 $DOTDIR/bin/$1	
-}
+# https://github.com/github/gitignore
+#echo "global .gitignore"
+#git config --global core.excludesfile ~/.gitignore
+
+#download_bin() {
+#	echo "downloading $1"
+#	curl -sL $2 > $DOTDIR/bin/$1
+#	chmod 755 $DOTDIR/bin/$1	
+#}
 
 # https://github.com/djl/vcprompt
 #download_bin vcprompt "https://github.com/djl/vcprompt/raw/master/bin/vcprompt"
 #download_bin dropbox.py "https://www.dropbox.com/download?dl=packages/dropbox.py"
 
-echo "echo '. $DOTDIR/bashrc' >> ~/.bashrc"
+echo '. $DOTDIR/bashrc' >> ~/.bashrc

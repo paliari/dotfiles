df0aa8d0b1f6ae9c919a74f04708f1140ef758ec
Marcos Neves <marcos.neves@gmail.com>
2015-10-21 11:02:14 -0200

    Alias gs only if gs does not exists

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -3,7 +3,13 @@ alias gp="git push"
 alias gd="git icdiff"
 # alias gs="git status" # conflict with GhostScript
 alias gt="git status"
-ga() { git add -A $* && git status }
+
+# utilizar alias gs apenas se não estiver instalado o GhostScript
+unalias gs
+[[ $(which gs) ]] || alias gs="git status"
 
 unalias gc
 gc() { git commit -m "$*" && git status }
+
+unalias ga
+ga() { git add -A $* && git status }
